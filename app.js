a3550844a0b42a314aa96e49bb546cdd89d51c8a
Fanghan Hu <Fanghan.Hu@gmail.com>
2020-11-30 19:06:35 -0600

    output file

--- app.js
+++ app.js
@@ -14,22 +14,90 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+let employees = [];
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function promptQuestions() {
+    inquirer.prompt([
+        {
+            name: 'choice',
+            type: "list",
+            message: "What type of team member would you like to add?",
+            choices:['manager', 'engineer', 'intern', "No more"],
+        }
+    ]).then(response => {
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+        let questions = [
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is this teammate's name?"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is this teammate's id?"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is this teammate's email?"
+            }
+        ];
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+        // noinspection JSUnresolvedVariable
+        switch (response.choice) {
+            case 'manager':
+                questions.push({
+                    type: 'input',
+                    name: 'officeNumber',
+                    message: "What is this teammate's office number?"
+                },);
+                break;
+            case 'engineer':
+                questions.push({
+                    type: 'input',
+                    name: 'github',
+                    message: "What is this teammate's github?"
+                },);
+                break;
+            case 'intern':
+                questions.push({
+                    type: 'input',
+                    name: 'school',
+                    message: "What is this teammate's school?"
+                },);
+                break;
+            case "No more":
+                //render into html.
+                let html = render(employees);
+                //check if folder exists, create new folder if it doesn't exist
+                if(!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR);
+                //write html into file, log error if error exists
+                fs.writeFile(outputPath, html, err => err?console.error(err):"");
+
+                //break from recursive loop.
+                return;
+        }
+
+        inquirer.prompt(questions).then(answers => {
+            let {name, id, email} = answers;
+            let commonFields = [name, id, email];
+            // noinspection JSUnresolvedVariable
+            switch (response.choice) {
+                case 'manager':
+                    employees.push(new Manager(...commonFields, answers.officeNumber));
+                    break;
+                case 'engineer':
+                    employees.push(new Engineer(...commonFields, answers.github));
+                    break;
+                case 'intern':
+                    employees.push(new Intern(...commonFields, answers.school));
+                    break;
+            }
+            promptQuestions();
+        })
+    });
+}
+
+
+promptQuestions();
